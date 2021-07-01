60af22f0fb5c4f656b7236e60fcf4511c693e111
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-07-01 19:39:23 +0200

    Tested single query lifecycle.

--- src/Composed.Query.Tests/QueryTests.cs
+++ src/Composed.Query.Tests/QueryTests.cs
@@ -1,8 +1,10 @@
 namespace Composed.Query.Tests
 {
     using System;
+    using System.Drawing;
     using System.Threading.Tasks;
     using Composed.Query.Tests.Utilities;
+    using Shouldly;
     using Xunit;
 
     public class QueryTests
@@ -21,9 +23,38 @@ namespace Composed.Query.Tests
             () => throw new InvalidOperationException(),
         };
 
+        #region Deconstruct Tests
+
+        [Fact]
+        public void Deconstruct_QueryAndState_Deconstructs()
+        {
+            var client = new QueryClient();
+            using var originalQuery = client.CreateQuery(GetKey(), () => Task.FromResult(0));
+            var (query, state) = originalQuery;
+
+            query.ShouldBeSameAs(originalQuery);
+            state.ShouldBeSameAs(originalQuery.State);
+        }
+
+        #endregion
+
+        #region ToString Tests
+
+        [Fact]
+        public void ToString_ReturnsQueryStateStringRepresentation()
+        {
+            var client = new QueryClient();
+            using var query = client.CreateQuery(GetKey(), () => Task.FromResult(0));
+            query.ToString().ShouldBe(query.State.Value.ToString());
+        }
+
+        #endregion
+
+        #region Initialization Tests
+
         [Theory]
         [MemberData(nameof(EnabledQueryKeyProviderData))]
-        public void Constructor_EnabledQueryKeyProvider_InitializesQueryInLoadingState(QueryKeyProvider queryKeyProvider)
+        public void Initialization_EnabledQueryKeyProvider_InitializesQueryInLoadingState(QueryKeyProvider queryKeyProvider)
         {
             var controller = new QueryFunctionController<int>();
             var client = new QueryClient();
@@ -35,7 +66,7 @@ namespace Composed.Query.Tests
 
         [Theory]
         [MemberData(nameof(DisabledQueryKeyProviderData))]
-        public void Constructor_DisabledQueryKeyProvider_InitializesQueryInDisabledState(QueryKeyProvider queryKeyProvider)
+        public void Initialization_DisabledQueryKeyProvider_InitializesQueryInDisabledState(QueryKeyProvider queryKeyProvider)
         {
             var controller = new QueryFunctionController<int>();
             var client = new QueryClient();
@@ -45,31 +76,75 @@ namespace Composed.Query.Tests
             controller.Verify(0);
         }
 
+        #endregion
+
+        #region Query Flows
+
         [Fact]
-        public async Task SingleEnabledQuery_OnQueryFunctionCompletion_HasData()
+        public async Task SingleQuery_OnQueryFunctionCompletion_CorrectlyEntersAllStates()
         {
             var controller = new QueryFunctionController<int>();
             var client = new QueryClient();
             using var query = client.CreateQuery(GetKey(), controller.Function);
+            var error = new Exception();
 
+            // -> Loading
             query.ShouldBeInLoadingState(key: GetKey());
             controller.Verify(1);
 
+            // Loading -> Success
             await controller.ReturnAndWaitForStateChange(query, 123);
 
             query.ShouldBeInSuccessState(key: GetKey(), data: 123);
             controller.Verify(1);
 
+            // Success -> FetchingSuccess
             controller.Reset();
             query.Refetch();
 
             query.ShouldBeInFetchingSuccessState(key: GetKey(), data: 123);
             controller.Verify(2);
 
+            // FetchingSuccess -> Success
             await controller.ReturnAndWaitForStateChange(query, 456);
 
             query.ShouldBeInSuccessState(key: GetKey(), data: 456);
             controller.Verify(2);
+
+            // Success -> FetchingSuccess
+            controller.Reset();
+            query.Refetch();
+
+            query.ShouldBeInFetchingSuccessState(key: GetKey(), data: 456);
+            controller.Verify(3);
+
+            // FetchingSuccess -> Error
+            await controller.ThrowAndWaitForStateChange(query, error);
+
+            query.ShouldBeInErrorState(key: GetKey(), error);
+            controller.Verify(3);
+
+            // Error -> FetchingError
+            controller.Reset();
+            query.Refetch();
+
+            query.ShouldBeInFetchingErrorState(key: GetKey(), error);
+            controller.Verify(4);
+
+            // FetchingError -> Success
+            await controller.ReturnAndWaitForStateChange(query, 789);
+
+            query.ShouldBeInSuccessState(key: GetKey(), 789);
+            controller.Verify(4);
+
+            // Success -> Disabled
+            query.Dispose();
+            query.Refetch(); // Should not change the state after disposal.
+
+            query.ShouldBeInDisabledState();
+            controller.Verify(4);
         }
+
+        #endregion
     }
 }
